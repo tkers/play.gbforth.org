7c5abdea11d672fd47ff9ee89bc75ba3e39eff5b
Tijn Kersjes <tijn@divbyzero.nl>
2020-10-19 21:03:02 +0200

    Add Sokoban, Simon, Aces Up, Happy Birthday ROMs

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+const getRom = name => {
+  switch (name) {
+    case "soko":
+      return "sokoban";
+    case "simon":
+      return "simon";
+    case "aces":
+      return "aces-up";
+    case "bday":
+      return "happy-birthday";
+    default:
+      return "hello";
+  }
+};
+
 window.addEventListener("load", () => {
-  playFromUrl("roms/hello.gb", document.getElementById("lcd"));
+  const name = window.location.hash.substring(1);
+  const url = `roms/${getRom(name)}.gb`;
+  playFromUrl(url, document.getElementById("lcd"));
 });
+
+window.addEventListener("hashchange", () => {
+  window.location.reload();
+}, false);
